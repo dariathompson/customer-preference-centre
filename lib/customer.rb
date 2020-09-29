ea62c57fd4bbd10c5a799451c6bea4e4abdc36d0
Daria <daria.kambalina@gmail.com>
2020-09-29 14:43:37 +0100

    Test for storing customer's name passes

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,2 +1,6 @@
 class Customer
+    attr_reader :name
+    def initialize(name:)
+        @name = name
+    end
 end
--- spec/customer_spec.rb
+++ spec/customer_spec.rb
@@ -1,4 +1,10 @@
 require 'customer'
 
 describe Customer do
+    let(:customer) { described_class.new(name: 'Daria')}
+    describe '#initialize' do
+        it 'stores name of a customer' do
+            expect(customer.name).to eq 'Daria'
+        end
+    end
 end
