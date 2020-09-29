e76eb29cae4d040159c8a78b2a2ed425ae5ae6d7
Daria <daria.kambalina@gmail.com>
2020-09-29 19:32:32 +0100

    Report doesn't include customers who chose to never recieve marketing info

--- spec/report_spec.rb
+++ spec/report_spec.rb
@@ -7,6 +7,7 @@ describe Report do
   let(:andy) { double :customer, name: "Andy", preferred_dates: "Mon" }
   let(:alex) { double :customer, name: "Alex", preferred_dates: "Mon, Fri" }
   let(:christina) { double :customer, name: "Christina", preferred_dates: "01, 13, 20" }
+  let(:zoya) { double :customer, name: "Zoya", preferred_dates: "never" }
   let(:customers) { [] }
   let(:report) { described_class.new(customers) }
   let(:date) { Date.today.strftime('%a %d-%B-%Y') }
@@ -26,7 +27,7 @@ describe Report do
     let(:weekday_report) { described_class.new(chosen_weekday) } 
     let(:chosen_weekdays) { [alex] }
     let(:weekdays_report) { described_class.new(chosen_weekdays) } 
-    let(:chosen_dates) { [christina] }
+    let(:chosen_dates) { [christina, zoya] }
     let(:dates_report) { described_class.new(chosen_dates) } 
 
     it 'prints next 90 days' do
@@ -70,6 +71,13 @@ describe Report do
       dates_report.print_dates
       output = $stdout.string.split(" ")
       expect(output).to include("Christina")
+    end
+
+    it "does not include customer if they chose 'never'" do
+      $stdout = StringIO.new
+      dates_report.print_dates
+      output = $stdout.string.split(" ")
+      expect(output).not_to include("Zoya")
     end
   end
 end
