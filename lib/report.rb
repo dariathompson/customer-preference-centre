40593f3108e4ff3b5c017bfb3336916101e53c02
Daria <daria.kambalina@gmail.com>
2020-09-29 19:04:55 +0100

    Report includes customers who chose preffered date

--- lib/report.rb
+++ lib/report.rb
@@ -10,8 +10,13 @@ class Report
   def print_dates
     counter = 0
     90.times do
+      current_day = (Date.today + counter).strftime('%a-%d-%B-%Y')
       print "\n"+ (Date.today + counter).strftime('%a %d-%B-%Y')
       everyday
+      arr = current_day.split("-")
+      @customers.map do
+        |customer| print ' ' + customer.name if customer.preferred_dates == arr[1]
+      end
       counter += 1
     end
   end
