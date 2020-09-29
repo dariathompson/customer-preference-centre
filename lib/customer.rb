0be08d01434d8411490f3b8a1d5bb047a1247564
Daria <daria.kambalina@gmail.com>
2020-09-29 14:46:37 +0100

    Test for storing customer's preffered dates passes

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,6 +1,7 @@
 class Customer
-    attr_reader :name
-    def initialize(name:)
+    attr_reader :name, :preferred_dates
+    def initialize(name:, preferred_dates:)
         @name = name
+        @preferred_dates = preferred_dates
     end
 end
--- spec/customer_spec.rb
+++ spec/customer_spec.rb
@@ -1,10 +1,13 @@
 require 'customer'
 
 describe Customer do
-    let(:customer) { described_class.new(name: 'Daria')}
+    let(:customer) { described_class.new(name: 'Daria', preferred_dates: 'Mon, Tue')}
     describe '#initialize' do
-        it 'stores name of a customer' do
+        it 'stores the name of a customer' do
             expect(customer.name).to eq 'Daria'
+        end
+        it 'stores the preferred dates to receive marketing information' do
+            expect(customer.preferred_dates).to eq 'Mon, Tue'
         end
     end
 end
