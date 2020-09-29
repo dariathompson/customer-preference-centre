c1e0faf6f8d49c2f78c95776acaadfd87eeb45ca
Daria <daria.kambalina@gmail.com>
2020-09-29 15:49:11 +0100

    User can see options of preferred dates

--- lib/preference_centre.rb
+++ lib/preference_centre.rb
@@ -1,13 +1,21 @@
 class PreferenceCentre
   attr_reader :customers, :name, :preferred_dates
   def initialize
-		@customers = []
-		@name = ''
-		@preferred_dates = ''
-	end
-	
-	def add_name
-		puts "Type your name, please"
-		@name = gets.chomp
-	end
+    @customers = []
+    @name = ''
+    @preferred_dates = ''
+  end
+
+  def add_name
+    puts 'Type your name, please'
+    @name = gets.chomp
+  end
+
+  def show_options
+    puts 'When would you like to receive marketing info?'
+    puts 'Type 1 if on a specified date of the month (1-28)'
+    puts 'Type 2 if on each specified day of the week [MON-SUN]'
+    puts 'Type 3 if every day'
+    puts 'Type 4 if never'
+  end
 end
--- spec/preference_centre_spec.rb
+++ spec/preference_centre_spec.rb
@@ -2,15 +2,16 @@ require 'preference_centre'
 require 'stringio'
 
 describe PreferenceCentre do
+  let(:centre) { described_class.new }
   describe '#initialize' do
     it 'initializes with an empty list of customers' do
-      expect(subject.customers).to eq []
+      expect(centre.customers).to eq []
     end
     it "initializes with an empty customer's name" do
-      expect(subject.name).to eq ''
+      expect(centre.name).to eq ''
     end
     it "initializes with an empty customer's preffered dates" do
-      expect(subject.preferred_dates).to eq ''
+      expect(centre.preferred_dates).to eq ''
     end
   end
 
@@ -18,9 +19,21 @@ describe PreferenceCentre do
     let(:input) { StringIO.new('Daria') }
     it "stores user's input as a name" do
       $stdin = input
-      expect { subject.add_name }.to output(
-          "Type your name, please\n"
-      ).to_stdout.and change { subject.name }.to('Daria')
+      expect { centre.add_name }.to output(
+        "Type your name, please\n"
+      ).to_stdout.and change { centre.name }.to('Daria')
+    end
+  end
+
+  describe '#show_options' do
+    it 'Gives options to choose preferred dates' do
+      expect { centre.show_options }.to output(
+        "When would you like to receive marketing info?
+Type 1 if on a specified date of the month (1-28)
+Type 2 if on each specified day of the week [MON-SUN]
+Type 3 if every day
+Type 4 if never\n"
+      ).to_stdout
     end
   end
 end
