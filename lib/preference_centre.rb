bfd40e60055c8f651c29ba9f23dd5fe6ab3979b3
Daria <daria.kambalina@gmail.com>
2020-09-29 16:04:40 +0100

    User can choose to never receive marketing information

--- lib/preference_centre.rb
+++ lib/preference_centre.rb
@@ -10,12 +10,20 @@ class PreferenceCentre
     puts 'Type your name, please'
     @name = gets.chomp
   end
+	
+	def choose_dates
+		show_options
+		user_input = gets.chomp
+		if user_input == '4'
+			@preferred_dates = 'never'
+		end
+	end
 
-  def show_options
+	def show_options
     puts 'When would you like to receive marketing info?'
     puts 'Type 1 if on a specified date of the month (1-28)'
     puts 'Type 2 if on each specified day of the week [MON-SUN]'
     puts 'Type 3 if every day'
     puts 'Type 4 if never'
-  end
+	end
 end
