8f0a6074044638902cf10ea1331a81f904ede4e7
Daria <daria.kambalina@gmail.com>
2020-09-29 18:07:47 +0100

    Report prints next 90 days

--- lib/report.rb
+++ lib/report.rb
@@ -1,8 +1,17 @@
 require_relative 'preference_centre'
+require 'date'
 
 class Report
   attr_reader :customers
-    def initialize(customers)
-        @customers = customers
+  def initialize(customers)
+    @customers = customers
+  end
+
+  def print_dates
+    counter = 0
+    90.times do
+      puts (Date.today + counter).strftime('%a %d-%B-%Y')
+      counter += 1
     end
+  end
 end
